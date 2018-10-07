6333b60da9af7ee904b08bdb3e1f84e64ea0f0d3
Mariusmivw <mariusmivw@gmail.com>
2018-10-07 09:48:47 +0200

    followed a tutorial

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const PORT = process.env.PORT || 8000;
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+	socket.broadcast.emit("chat message", "A user connected!");
+	
+	socket.on("disconnect", function() {
+		socket.broadcast.emit("chat message", "A user disconnected!");
+	});
+	
+	socket.on("chat message", function (msg) {
+		console.log("message: " + msg);
+		// io.emit("chat message", msg); // everyone including sender
+		socket.broadcast.emit("chat message", msg); // everyone excluding sender
+	});
+
+	// template
+	// socket.on("{event name}", function ({parameters}) {
+		// thing to do
+	// });
+});
+
+http.listen(PORT, function() {
+	console.log(`listening on port: ${PORT}`);
+});
